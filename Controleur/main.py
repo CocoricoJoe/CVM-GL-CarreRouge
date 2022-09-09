d007ab7466429fe12d91546f2c0fc864a3c28de2
Justin Mayer <mayer.justin1@gmail.com>
2022-09-09 12:10:39 -0400

    added lines

---
+++ Controleur/main.py
@@ -0,0 +1,10 @@
+import c31Geometry2 as geometry
+import tkinter as tk
+
+if __name__ == "__main__":
+    #Créer la fenetre de base
+    root = tk.Tk()
+    root.title("Jeu du carré rouge")
+    root.geometry("800x800")
+    root.mainloop()
+    
