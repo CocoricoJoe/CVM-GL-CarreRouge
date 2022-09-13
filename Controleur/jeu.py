32a58b78b2e8a219cb848af0dbd784117bf1b37e
Justin Mayer <mayer.justin1@gmail.com>
2022-09-13 08:44:42 -0400

    added lines

--- Controleur/jeu.py
+++ Controleur/jeu.py
@@ -9,20 +9,31 @@ if __name__ == "__main__" :
     # Ajouter un titre à la fenêtre
     root.title("Jeu.py")
     # Modifier la taille
-    root.geometry("800x800")
+    root.geometry("600x600")
     
 
     # HARDCODE
-    vecCarreBlanc = g.Vecteur(300,300)
-    vecCarreRouge = g.Vecteur(300,300)
-    vecRectangle1 = g.Vecteur(150,150)
+    vecCarreNoir  = g.Vecteur(250,250)
+    vecCarreBlanc = g.Vecteur(225,225)
+    vecCarreRouge = g.Vecteur(225,225)
+    vecRectangle1 = g.Vecteur(100,100)
+    vecRectangle2 = g.Vecteur(300,85)
+    vecRectangle3 = g.Vecteur(85,300)
+    vecRectangle4 = g.Vecteur(355,340)
     
     
-    aireDeJeu = tk.Canvas(root, background="black", height="600", width="600") 
+    
+    aireDeJeu = tk.Canvas(root, background="white", height="500", width="500") 
+    carreNoir = g.Carre(aireDeJeu,vecCarreNoir,500,0,remplissage="black",bordure="black")
     carreBlanc = g.Carre(aireDeJeu,vecCarreBlanc,450,0,remplissage="white",bordure="white")
-    carreRouge = g.Carre(aireDeJeu,vecCarreRouge,50,0,remplissage="red",bordure="red")
-    rectangle1 = g.Rectangle(aireDeJeu,vecRectangle1,50,10,0,remplissage="blue",bordure="blue")
+    carreRouge = g.Carre(aireDeJeu,vecCarreRouge,40,0,remplissage="red",bordure="red")
+    rectangle1 = g.Rectangle(aireDeJeu,vecRectangle1,60,60,0,remplissage="blue",bordure="blue")
+    rectangle2 = g.Rectangle(aireDeJeu,vecRectangle2,60,50,0,remplissage="blue",bordure="blue")
+    rectangle3 = g.Rectangle(aireDeJeu,vecRectangle3,30,600,0,remplissage="blue",bordure="blue")
+    rectangle4 = g.Rectangle(aireDeJeu,vecRectangle4,100,10,0,remplissage="blue",bordure="blue")
+    
     aireDeJeu.grid()
+    carreNoir.draw()
     carreBlanc.draw()
     carreRouge.draw()
     rectangle1.draw()
