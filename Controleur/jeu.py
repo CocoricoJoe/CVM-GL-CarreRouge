edad61f776b385d72ef71dc3087ef92fe8cf3f06
Justin Mayer <mayer.justin1@gmail.com>
2022-09-16 11:36:13 -0400

    merge1

--- Controleur/jeu.py
+++ Controleur/jeu.py
@@ -1,13 +1,14 @@
-from cProfile import label
-from pyparsing import col
+from msilib.schema import Class
 import c31Geometry2 as g
 import tkinter as tk
 from datetime import date
 import csv
+from functools import partial
+import partie as p
 
-def LireScore(): 
+def LireScore(root): 
 
-    filePath = "C:/travail/CVM-GL-CarreRouge/Controleur/stats.csv"
+    filePath = "X:/GL/CVM-GL-CarreRougeDEV/Controleur/stats.csv"
     file = open(filePath) 
     reader = csv.reader(file)
     Data = list(reader)
@@ -17,76 +18,161 @@ def LireScore():
         listbox.insert(x,y)
 
     listbox.grid(column=0,row=0)
-    
-    
+
+def EffacerScore():
+    filePath = "X:/GL/CVM-GL-CarreRougeDEV/Controleur/stats.csv"
+    file = open(filePath, "w" )
+    file.truncate()
+    file.close()
 
 def EcrireScore(array):
     
-   filePath = "C:/travail/CVM-GL-CarreRouge/Controleur/stats.csv" 
-   file = open(filePath)
+   filePath = "X:/GL/CVM-GL-CarreRougeDEV/Controleur/stats.csv" 
+   file = open(filePath, "w")
    writer = csv.writer(file)    
-   for partie in array:
-        writer.writerow(partie.nom)
+   for item in array:
+        writer.writerow(item.nom)
 
-def NouvellePartie(root):
+class Jeu:
+     
+    vecCarreNoir = 0
+    vecCarreBlanc = 0
+    vecCarreRouge = 0
+    vecRectangle1 = 0
+    vecRectangle2  = 0
+    vecRectangle3 = 0
+    vecRectangle4 = 0
+    velocite = 0
+    aireDeJeu = 0
+    carreNoir = 0
+    carreBlanc = 0
+    carreRouge = 0
+    etatCarre = "Vivant"
+    rectangle1 = 0
+    rectangle2 = 0
+    rectangle3 = 0
+    rectangle4 = 0
+    rectangles = 0
+    boutonNouvellePartie = 0
+    boutonNouvelleSession = 0
+    boutonAfficherScore = 0
+    boutonEffacerScore = 0
+    nomJoueur = 0
+    champNom = 0
+    scoreSession = 0
+    partie = 0
     
-    vecCarreNoir  = g.Vecteur(225,225)
-    vecCarreBlanc = g.Vecteur(225,225)
-    vecCarreRouge = g.Vecteur(225,225)
-    vecRectangle1 = g.Vecteur(100,100)
-    vecRectangle2 = g.Vecteur(300,85)
-    vecRectangle3 = g.Vecteur(85,300)
-    vecRectangle4 = g.Vecteur(355,340)
-    
-    aireDeJeu = tk.Canvas(root, background="white", height="450", width="450") 
-    carreNoir = g.Carre(aireDeJeu,vecCarreNoir,450,0,remplissage="black",bordure="black")
-    carreBlanc = g.Carre(aireDeJeu,vecCarreBlanc,350,0,remplissage="white",bordure="white")
-    carreRouge = g.Carre(aireDeJeu,vecCarreRouge,40,0,remplissage="red",bordure="red")
-    rectangle1 = g.Rectangle(aireDeJeu,vecRectangle1,60,60,0,remplissage="blue",bordure="blue")
-    rectangle2 = g.Rectangle(aireDeJeu,vecRectangle2,60,50,0,remplissage="blue",bordure="blue")
-    rectangle3 = g.Rectangle(aireDeJeu,vecRectangle3,30,60,0,remplissage="blue",bordure="blue")
-    rectangle4 = g.Rectangle(aireDeJeu,vecRectangle4,100,10,0,remplissage="blue",bordure="blue")
-    boutonNouvellePartie = tk.Button(root, text = "Nouvelle partie", command=NouvellePartie)
-    boutonNouvelleSession = tk.Button(root, text = "Nouvelle session", command=NouvelleSession)
-    boutonAfficherScore = tk.Button(root, text = "Afficher les scores", command=LireScore)
-    nomJoueur = tk.StringVar(root)
-    champNom = tk.Entry(root,textvariable=nomJoueur,width=30)
-    champNom.insert(0,"Entrez un nom d'utilisateur")
-    
-    aireDeJeu.grid(column=0,row=0)
-    boutonNouvellePartie.grid(column=0,row=2)
-    boutonNouvelleSession.grid(column=0,row=3)
-    champNom.grid(column=0, row=1)
-    boutonAfficherScore.grid(column=0, row=4)
-    carreNoir.draw()
-    carreBlanc.draw()
-    carreRouge.draw()
-    rectangle1.draw()
-    rectangle2.draw()
-    rectangle3.draw()
-    rectangle4.draw()
-    
-def NouvelleSession():
-    NouvellePartie()
-    partie = Partie(1000,"FuckTheo")
-    scoresSession = []
-    scoresSession.append(partie)
-    EcrireScore(scoresSession)
+    def NouvellePartie(self,root,array):
+        
+        self.vecCarreNoir  = g.Vecteur(225,225)
+        self.vecCarreBlanc = g.Vecteur(225,225)
+        self.vecCarreRouge = g.Vecteur(225,225)
+        self.vecRectangle1 = g.Vecteur(100,100)
+        self.vecRectangle2 = g.Vecteur(300,85)
+        self.vecRectangle3 = g.Vecteur(85,300)
+        self.vecRectangle4 = g.Vecteur(355,340)
+        
+        self.velocite = 1
+        
+        self.aireDeJeu = tk.Canvas(root, background="white", height="450", width="450") 
+        self.carreNoir = g.Carre(self.aireDeJeu,self.vecCarreNoir,450,0,remplissage="black",bordure="black")
+        self.carreBlanc = g.Carre(self.aireDeJeu,self.vecCarreBlanc,350,0,remplissage="white",bordure="white")
+        self.carreRouge = g.Carre(self.aireDeJeu,self.vecCarreRouge,40,0,remplissage="red",bordure="red")
+        self.rectangle1 = g.Rectangle(self.aireDeJeu,self.vecRectangle1,60,60,0,remplissage="blue",bordure="blue")
+        self.rectangle2 = g.Rectangle(self.aireDeJeu,self.vecRectangle2,60,50,0,remplissage="blue",bordure="blue")
+        self.rectangle3 = g.Rectangle(self.aireDeJeu,self.vecRectangle3,30,60,0,remplissage="blue",bordure="blue")
+        self.rectangle4 = g.Rectangle(self.aireDeJeu,self.vecRectangle4,100,10,0,remplissage="blue",bordure="blue")
+        self.rectangles = [self.rectangle1,self.rectangle2,self.rectangle3,self.rectangle4]
+        self.boutonNouvellePartie = tk.Button(root, text = "Nouvelle partie", command=partial(self.NouvellePartie,root,array))
+        self.boutonNouvelleSession = tk.Button(root, text = "Nouvelle session", command=partial(self.NouvelleSession,root))
+        self.boutonAfficherScore = tk.Button(root, text = "Afficher les scores", command=partial(LireScore,root))
+        self.boutonEffacerScore = tk.Button(root,text = "Effacer les scores", command=partial(EffacerScore))
+        self.nomJoueur = tk.StringVar(root)
+        self.champNom = tk.Entry(root,textvariable=self.nomJoueur,width=30)
+        self.champNom.insert(0,"Entrez un nom d'utilisateur")
+        
+        self.aireDeJeu.grid(column=0,row=0)
+        self.boutonNouvellePartie.grid(column=0,row=2)
+        self.boutonNouvelleSession.grid(column=0,row=3)
+        self.champNom.grid(column=0, row=1)
+        self.boutonAfficherScore.grid(column=0, row=4)
+        self.boutonEffacerScore.grid(column=0,row=5)
+        self.carreNoir.draw()
+        self.carreBlanc.draw()
+        self.carreRouge.draw()
+        self.rectangle1.draw()
+        self.rectangle2.draw()
+        self.rectangle3.draw()
+        self.rectangle4.draw()
+        
+    def NouvelleSession(self,root):
+        
+        self.scoresSession = []
+        self.partie = p.Partie(1000,"je suis cavolo")
+        #self.scoresSession.append(partie)
+        #EcrireScore(scoresSession)
+        self.NouvellePartie(root,self.scoresSession)
+        
+    # Collisions 
+    def collisionRectangle(self, rectangle:g.Polygone):
+        colli = ''
+        points = rectangle.get_coordonnees()
+        xPoints = [point[0] for point in points]
+        yPoints = [point[1] for point in points]
+        hautRectangle = min(yPoints)
+        gaucheRectangle = min(xPoints)
+        basRectangle = max(yPoints)
+        droiteRectangle = max(xPoints)
+        
+        if hautRectangle <= 0 or basRectangle >= 450:
+            colli = 'Y'
+        if gaucheRectangle <= 0 or droiteRectangle >= 449:
+            colli = 'X'
+        
+        return colli
 
-class Partie:
-    
-    pointage = 0
-    nom = "NA"
-    dateJeu = 0
-    
-    
-    def __init__(self,score,nom="NA"):
-        self.pointage = score
-        self.nom = nom
-        self.dateJeu = date.today()
-  
-    
+
+    def collisionCarre(self):
+        collision = False
+        centreCarre = self.carreRouge.get_barycentre()
+        
+        for rectangle in self.rectangles:
+            for point in rectangle.get_coordonnees():
+                if (g.Vecteur(point[0],point[1]).distance(centreCarre) <= 20):
+                    self.carreRouge.set_remplissage("black")  
+                    collision = True  
+                                        
+        return collision
 
 
+        
+    # Déplacements
+    def deplacementRectangles(self):
+        for rectangle in self.rectangles:
+            
+            if self.collisionRectangle(rectangle) == 'X':
+                rectangle.sensX *= -1
+            if self.collisionRectangle(rectangle) == 'Y':
+                rectangle.sensY *= -1                
+                
+            nouvellePosition = (rectangle.get_barycentre() + g.Vecteur(self.velocite*rectangle.sensX,self.velocite*rectangle.sensY)) - rectangle.get_barycentre()                
+            rectangle.translate(nouvellePosition)
+            rectangle.draw()   
+            
 
-    
+    def deplacementCarreRouge(self, e):
+        x= e.x
+        y= e.y
+        nouvellePosition = g.Vecteur(x,y) - self.carreRouge.get_barycentre()
+        self.carreRouge.translate(nouvellePosition)    
+            
+        if self.collisionCarre():
+            self.etatCarre = "Mort"
+        
+        print(self.etatCarre)            
+        self.carreRouge.draw()
+            
+            
+    # Actualiser le jeu
+    def update(self):
+            self.deplacementRectangles()
--- Controleur/main.py
+++ Controleur/main.py
@@ -1,17 +1,17 @@
+from jeu import Jeu
 import c31Geometry2 as geometry
 import tkinter as tk
 import jeu as j
 
 if __name__ == "__main__" :
     
-    
+    jeu = Jeu()
     root = tk.Tk()
     root.title("Jeu.py")
     root.geometry("450x600")
 
-    j.NouvellePartie(root)
+    jeu.NouvelleSession(root)
+    print(jeu.partie.nom)
     
-    root.mainloop()   
-    
-   
-        
+    root.bind('<Motion>', jeu.deplacementCarreRouge)
+    root.mainloop()
